7791ba78022bb2aa299e9ed93a1146aa4a35426c
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-03-25 14:12:44 -0400

    Render a list of all reserved rockets using filter method on the My profile page
    -Update App.js, store.js, MyProfile.js
    -Debug linters

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
-function MyProfile() {
+const MyProfile = ({ rockets }) => {
+  const reservedItems = (store) => store.filter((element) => element.reserved === true);
+
   return (
-    <div>
-      profile under construction
-    </div>
+    <section className="myprofile-container">
+      <div className="myprofile-rockets-container">
+        <h2 className="title-myprofile-rockets">My Rockets</h2>
+        <ul className="rockets-myprofile-list-ul">
+          {reservedItems(rockets).map((element) => (
+            <li className="rockets-myprofile-list-li" key={element.rocket_name}>{element.rocket_name}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
-}
+};
+
+MyProfile.propTypes = {
+  rockets: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
+};
 
 export default MyProfile;
